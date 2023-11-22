315d6734a946dd6e858778c843d6fa4ad9339e13
lpesce57 <113311610+lpesce57@users.noreply.github.com>
2023-11-22 06:17:55 -0600

    Update day_night.js

--- js/day_night.js
+++ js/day_night.js
@@ -4,21 +4,19 @@ window.addEventListener("load", function() {
 });
 
 function backgroundFunc() {
+  let body = document.body;
   let currentTime = new Date().getHours();
 
   //Helped assist with finding a variable to represent night times, all other are considered day
   const isNight = currentTime >= 18 || currentTime < 5;
 
-  //Helped me load my image in the right way, because it did not load the same way as the CSS or HTML images did
-  let night = new Image();
-  night.src = '../images/night.jpg';
-
-  let day = new Image();
-  day.src = '../images/day.jpg';
 
   if (isNight) {
-    document.querySelector("main").style.backgroundImage = night;
-  } else {
-    document.querySelector("main").style.backgroundImage = day;
+    body.classList.remove('day');
+    body.classList.add('night');
+  } 
+  else {
+    body.classList.remove('night');
+    body.classList.add('day');
   }
 }
